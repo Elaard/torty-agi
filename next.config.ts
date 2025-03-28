84b6196548a224c3924c5865e166537882cf9496
Elard <bartek.dv.89@gmail.com>
2025-03-28 13:49:36 +0100

    update next js config to recognize external img provider

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'torty-agi.s3.eu-north-1.amazonaws.com',
+        pathname: '/images/**',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
