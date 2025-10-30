963f19fea50786d7ced441baf2c5d399e5dd7365
Elard <bartek.dv.89@gmail.com>
2025-10-30 12:44:10 +0100

    remove aws dependencies

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,10 +1,6 @@
 export const routes = {
   home: '/',
-  login: '/logowanie',
   contact: '/kontakt',
-
   oferta: '/oferta?kategoria=all',
-  realizacje: '/realizacje',
-
-  adminPanel: '/panel-admina'
+  realizacje: '/realizacje'
 }
